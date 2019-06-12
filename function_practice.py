5d244eb674157c932421b54d621037e4ea5a0555
brandon vasquez <brandon.vasquez1001@gmail.com>
2019-06-12 11:47:29 -0500

    Added Fuction excersises

---
+++ function_practice.py
@@ -0,0 +1,20 @@
+"""
+
+title: function_practice
+author: Brandon
+date: 2019-06-12 11:21
+"""
+
+
+def age_calculator(year1, year2):
+    return year1 - year2
+
+
+print(age_calculator(2019, 2004))
+
+
+def average_number(x1, x2, x3):
+    return (x1 + x2 + x3) / 3
+
+print(average_number(2,49,15))
+
