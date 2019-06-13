88e4e438e1d1fae6ef4eed157e84b4c35614f4bf
brandon vasquez <brandon.vasquez1001@gmail.com>
2019-06-13 11:03:55 -0500

    Added string practice labs

--- string_practice.py
+++ string_practice.py
@@ -6,13 +6,38 @@ date: 2019-06-11
 """
 # strings_lab
 
-answer = input("Enter a letter:  ")
-print(answer in "qwertyuiopasdfghjklzxcvbnm")
+#answer = input("Enter a letter:  ")
+#print(answer in "qwertyuiopasdfghjklzxcvbnm")
 
-short_hand = "Thank you for that! You are too sweet and kind"
-print(short_hand.replace("Thank", "Thnk"))
-print(short_hand.replace("you", "U"))
-print(short_hand.replace("for", "4"))
-print(short_hand.replace("that", "tht"))
-print(short_hand.replace())
+
+def is_letter(character):
+    return character.lower() in "qwertyuiopasdfghjklzxcvbnm"
+
+
+print(is_letter("isadfasdf"))
+print(is_letter("0"))
+
+def short_hand(short):
+    short = short.lower().replace("and", "&").replace("too", "2").replace("you", "u").replace("for", "4")
+    short = short.replace("a", "").replace("e", "").replace("i", "").replace("o", "")
+    return short
+print(short_hand("Thank you for that! You are too sweet and kind"))
+print(short_hand("How are you today"))
+
+
+def removing(check):
+    check = check.lower().replace("QWERTYUIOPASDFGHJKLZXCVBNM", "qwertyuiopasdfghjklzxcvbnm")
+    check = check.replace(",", " ").replace("'", " ").replace(" ", "")
+    return check
+
+
+print(removing("Madam, I'm Adam"))
+
+def palindrome(check):
+    check = removing(check)
+    return check == check[::-1]
+
+
+print(palindrome("Madam, I'm Adam"))
+print(palindrome("Computer"))
 
